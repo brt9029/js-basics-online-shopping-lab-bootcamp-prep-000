a41038dd36334230d59b6b1f2932993e79bab4ed
Bryant Torres <brt9029@gmail.com>
2018-06-16 17:42:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,23 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1
+  cart.push(Object.assign({itemName: item}, {itemPrice: price}))
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var cartStatus = "In your cart, you have "
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  } else if (cart.length > 0) {
+    var updated = []
+    for (let i = 0; i < cart.length; i++){
+      updated.push(Object.values(cart[i]))
+      updated = `${updated[i][0]} at $${updated[i][1]}.`
+    }
+    return updated
+  }
 }
 
 function total() {
